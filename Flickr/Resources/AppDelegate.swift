b55ea419c505ea9d908a30c6fcffd8a374039c50
Diogo Antunes <diogo.antunes@gmail.com>
2016-10-27 10:59:10 +0100

    Added Alamofire and SwiftyJSON as dependency.

---
+++ Flickr/Resources/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Flickr
+//
+//  Created by Diogo Antunes on 27/10/2016.
+//  Copyright © 2016 Diogo Antunes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    let flickrAPIKey = "4d4cebd14eb140fef1c2a30b67fe6da7"
+    let flickrAPISecret = "2aea7885596b727d"
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
